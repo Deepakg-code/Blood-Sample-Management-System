aa1f50f2467ebb4272b7d8e5cbde1cbd7d227b52
Deepak <deepakadhi0808@gmail.com>
2025-01-22 17:16:36 +0530

    updaing service and service impl class for updateUserById feature

--- src/main/java/com/jsp/bsm/service/UserService.java
+++ src/main/java/com/jsp/bsm/service/UserService.java
@@ -5,4 +5,9 @@ import com.jsp.bsm.entity.User;
 public interface UserService {
 
    public User addUser(User user);
+
+   public User findUserById(int userId);
+
+   public User updateUserById(int userId, User user);
+
 }
--- src/main/java/com/jsp/bsm/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/jsp/bsm/serviceimpl/UserServiceImpl.java
@@ -1,4 +1,52 @@
 package com.jsp.bsm.serviceimpl;
 
-public class UserServiceImpl {
+import com.jsp.bsm.entity.User;
+import com.jsp.bsm.exception.UserNotFoundExceptionById;
+import com.jsp.bsm.repository.UserRepository;
+import com.jsp.bsm.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserById(int userId) {
+        Optional<User> optional = userRepository.findById(userId);
+        if (optional.isEmpty())
+            throw new UserNotFoundExceptionById("User Not Found");
+        return optional.get();
+
+    }
+
+    @Override
+    public User updateUserById(int userId, User user) {
+        Optional<User> optional = userRepository.findById(userId);
+        if(optional.isPresent()){
+            User exuser = optional.get();
+            exuser.setUsername(user.getUsername());
+            exuser.setPhoneNumber(user.getPhoneNumber());
+            exuser.setAge(user.getAge());
+            exuser.setEmail(user.getEmail());
+            exuser.setPassword(user.getPassword());
+            exuser.setBloodGroup(user.getBloodGroup());
+            exuser.setGender(user.getGender());
+            exuser.setAvailableCity(user.getAvailableCity());
+            exuser.setVerified(user.isVerified());
+            return userRepository.save(exuser);
+        }
+        else {
+            throw new UserNotFoundExceptionById("User Not Get Updated");
+        }
+    }
 }
