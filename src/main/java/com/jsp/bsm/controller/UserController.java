4b321b03e3fe14a2f4df94ba6fd00c767343e56c
Deepak <deepakadhi0808@gmail.com>
2025-01-22 17:21:00 +0530

    updaing controller class for more efficent response to the user by using response structure and builder method

--- src/main/java/com/jsp/bsm/controller/UserController.java
+++ src/main/java/com/jsp/bsm/controller/UserController.java
@@ -3,24 +3,40 @@ package com.jsp.bsm.controller;
 import com.jsp.bsm.entity.User;
 import com.jsp.bsm.service.UserService;
 import com.jsp.bsm.utility.ResponseStructure;
+import com.jsp.bsm.utility.RestResponseBuilder;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class UserController {
 
-    @Autowired
-    private UserService userService;
 
+    private final UserService userService;
+
+    private  final RestResponseBuilder responseBuilder;
+
+    @PostMapping("/register")
     public ResponseEntity<ResponseStructure<User>> addUser(@RequestBody User user){
         user = userService.addUser(user);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(ResponseStructure.create(HttpStatus.CREATED.value(), "User Created", user));
+        return responseBuilder.success(HttpStatus.CREATED, "User Created", user);
     }
+
+    @GetMapping("/users/{userid}")
+    public ResponseEntity<ResponseStructure<User>> findUserById(@PathVariable("userid") int userId){
+        User user = userService.findUserById(userId);
+        return responseBuilder.success(HttpStatus.FOUND, "User Found", user);
+    }
+
+    @PutMapping("/users/{userid}")
+    public ResponseEntity<ResponseStructure<User>> updateUser(@PathVariable("userid") int userId, @RequestBody User user){
+        User user1 = userService.updateUserById(userId, user);
+        return responseBuilder.success(HttpStatus.OK, "User updated", user1);
+    }
+
 
 }
