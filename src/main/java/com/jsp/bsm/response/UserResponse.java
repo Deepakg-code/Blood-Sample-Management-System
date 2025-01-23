5201932ad75869c362dd2ea066062f4dbfe1ee36
Deepak <deepakadhi0808@gmail.com>
2025-01-23 12:39:50 +0530

    Creating UserResponse class for achiving Data Transfer Object

---
+++ src/main/java/com/jsp/bsm/response/UserResponse.java
@@ -0,0 +1,28 @@
+package com.jsp.bsm.response;
+
+import com.jsp.bsm.enums.BloodGroup;
+import com.jsp.bsm.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+
+    private int userId;
+    private String username;
+    private BloodGroup bloodGroup;
+    private LocalDate lastDonatedAt;
+    private int age;
+    private Gender gender;
+    private String availableCity;
+    private boolean verified;
+
+
+}
