e0fdfe978ab051e1f52e3242cb25eb0ec632acff
Deepak <deepakadhi0808@gmail.com>
2025-01-24 01:18:00 +0530

    updating repository class by removing annotaion

--- src/main/java/com/jsp/bsm/repository/HospitalRepository.java
+++ src/main/java/com/jsp/bsm/repository/HospitalRepository.java
@@ -4,6 +4,5 @@ import com.jsp.bsm.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
 }
