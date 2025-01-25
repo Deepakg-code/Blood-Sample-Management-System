984dd3ab44d0de5ce36a8916230785e2ebc4c484
Deepak <deepakadhi0808@gmail.com>
2025-01-25 23:36:47 +0530

    Updating UserService for adding directky user as admin

--- src/main/java/com/jsp/bsm/service/UserService.java
+++ src/main/java/com/jsp/bsm/service/UserService.java
@@ -2,6 +2,7 @@ package com.jsp.bsm.service;
 
 import com.jsp.bsm.requestdto.UserRequest;
 import com.jsp.bsm.responsedto.UserResponse;
+import jakarta.validation.Valid;
 
 public interface UserService {
 
@@ -11,4 +12,5 @@ public interface UserService {
 
    public UserResponse updateUserById(int userId, UserRequest userRequest);
 
+   UserResponse addUserAsAdmin(@Valid UserRequest userRequest);
 }
