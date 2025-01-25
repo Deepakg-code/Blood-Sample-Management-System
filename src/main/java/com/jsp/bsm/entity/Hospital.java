cdc67b5fc539c9296885a3163c7b586b0aee0c74
Deepak <deepakadhi0808@gmail.com>
2025-01-25 23:31:30 +0530

    Updating Hospital  entity to have one to many relation with admin

--- src/main/java/com/jsp/bsm/entity/Hospital.java
+++ src/main/java/com/jsp/bsm/entity/Hospital.java
@@ -1,13 +1,12 @@
 package com.jsp.bsm.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -20,5 +19,8 @@ public class Hospital {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int hospitalId;
     private String name;
+
+    @OneToMany
+    private List<Admin> admin;
 
 }
