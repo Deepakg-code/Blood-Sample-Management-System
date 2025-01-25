790654bc100700e25df0b5f09745211cae278312
Deepak <deepakadhi0808@gmail.com>
2025-01-25 23:15:42 +0530

    Creating Admin repository for admin database interactions

---
+++ src/main/java/com/jsp/bsm/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.jsp.bsm.repository;
+
+import com.jsp.bsm.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+}
