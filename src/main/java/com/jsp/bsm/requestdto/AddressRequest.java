f155f7ca146ef44613b49502b93cf23708c9baff
Deepak <deepakadhi0808@gmail.com>
2025-01-27 22:42:39 +0530

    creating Address request and response dto for understanding respones

---
+++ src/main/java/com/jsp/bsm/requestdto/AddressRequest.java
@@ -0,0 +1,21 @@
+package com.jsp.bsm.requestdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressRequest {
+
+    private String addressLine;
+    private String landmark;
+    private String area;
+    private String city;
+    private String state;
+    private String country;
+    private int pincode;
+}
