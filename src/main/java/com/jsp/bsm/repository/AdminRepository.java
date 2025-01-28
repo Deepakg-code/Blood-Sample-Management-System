bde6075b4f01bdb229fb1fb7d16a4f26dcbed993
Deepak <deepakadhi0808@gmail.com>
2025-01-29 00:40:15 +0530

    updating AdminRepository to have custom method ti find user by Email

--- src/main/java/com/jsp/bsm/repository/AdminRepository.java
+++ src/main/java/com/jsp/bsm/repository/AdminRepository.java
@@ -3,5 +3,9 @@ package com.jsp.bsm.repository;
 import com.jsp.bsm.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+    public Optional<Admin> findByUser_Email(String userName);
 }
