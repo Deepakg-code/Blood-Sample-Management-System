1d39847700a832ee48592ca80d7ddb6b92e67e72
Deepak <deepakadhi0808@gmail.com>
2025-01-29 00:41:58 +0530

    updating BloodBank Repository to create the custom method to find bloodBank by given city and bloodgroup

--- src/main/java/com/jsp/bsm/repository/BloodRepository.java
+++ src/main/java/com/jsp/bsm/repository/BloodRepository.java
@@ -1,6 +1,7 @@
 package com.jsp.bsm.repository;
 
 import com.jsp.bsm.entity.BloodBank;
+import com.jsp.bsm.enums.BloodGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.Optional;
 
 public interface BloodRepository extends JpaRepository<BloodBank, Integer> {
 
-    public List<BloodBank> findByAddress_CityIn(List<String> cities);
+//    public List<BloodBank> findByAddress_CityIn(List<String> cities);
+List<BloodBank> findByAddress_CityInAndSamples_BloodGroup(List<String> cities, BloodGroup bloodGroup);
+
 }
