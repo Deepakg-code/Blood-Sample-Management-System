6f18767ee43fdb3c73582b60cb1345da37071616
Deepak <deepakadhi0808@gmail.com>
2025-02-04 00:42:52 +0530

    creating donation lead repository for database interactions

---
+++ src/main/java/com/jsp/bsm/repository/DonationLeadRepository.java
@@ -0,0 +1,7 @@
+package com.jsp.bsm.repository;
+
+import com.jsp.bsm.entity.DonationLead;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DonationLeadRepository extends JpaRepository<DonationLead, Integer> {
+}
